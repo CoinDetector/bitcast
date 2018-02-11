c92893d573ffffc1dfc7b2f9f2464392a06fbe3a
wldud26 <ljy6725@naver.com>
2018-02-11 16:48:34 +0900

    daum

--- daum_news.py
+++ daum_news.py
@@ -16,17 +16,6 @@ import sqlite3 as lite
 
 import re
 
-
-
-
-# https://news.google.com/news/search/section/q/가상화폐/가상화폐?hl=ko&gl=KR&ned=kr
-# https://news.google.com/news/headlines/section/topic/BUSINESS.ko_kr/경제?hl=ko&gl=KR
-
-# while True:
-# now = datetime.now()
-# print(time.)
-
-
 a = ['가상화폐','비트코인','규제','거래소 폐쇄','급락','하락','폭락','떡락','빗썸','압수수색','규제 강화']
 
 
@@ -43,30 +32,38 @@ cs.execute(query)
 
 while True:
     count = 0
+    j=0
+    c=0
+
     for i in range(len(a)):
-        r = requests.get('http://search.daum.net/search?w=news&q={0}'.format(a[i]))
-
-        soup = Soup(r.text, 'html.parser')
-        new_coll = soup.find(id='newsColl')
-        news_ul = new_coll.find(id="clusterResultUL")
-
-        for li in news_ul.find_all('li'):
-            date_tag = li.find(class_='f_nb date')
-            dateValue = date_tag.find('span', {"class":"txt_bar"})
-
-            p = re.compile('[0-5]+분') # re 내장모듈 내(.) compile 메서드를 사용.
-            m = p.search(dateValue.previous_sibling)
-
-            if m != None:
-               num = dateValue.previous_sibling
-               count += 1
+        for j in range(len(a)):
+            r = requests.get('http://search.daum.net/search?w=news&q={0}&p={1}'.format(a[i],j))
 
 
+            soup = Soup(r.text, 'html.parser')
+            new_coll = soup.find(id='newsColl')
+            news_ul = new_coll.find(id="clusterResultUL")
 
-    print("총 기사 갯수 :  {0}".format(count))
+
+            for li in news_ul.find_all('li'):
+                date_tag = li.find(class_='f_nb date')
+                dateValue = date_tag.find('span', {"class":"txt_bar"})
+
+                p = re.compile('[0-5]+분') # re 내장모듈 내(.) compile 메서드를 사용.
+                m = p.search(dateValue.previous_sibling)
+
+                if m != None:
+                   num = dateValue.previous_sibling
+                   count += 1
+
+                j += 1
 
 
-    time.sleep(3)
+
+
+        print("총 기사 갯수 :  {0}".format(count))
+
+        time.sleep(3)
 
 cs.close()
 conn.close()
