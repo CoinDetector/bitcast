97a20e47d9e6b19af7095443d710ef4d561a8547
Park <pbg6288@gmail.com>
2018-02-19 13:55:33 +0900

    google_search.py 최종

--- google_search.py
+++ google_search.py
@@ -3,125 +3,95 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import sqlite3 as lite
 
+keyword = ['비트코인','가상화폐 규제','거래소 폐쇄','급락','하락','폭락','떡락','빗썸','압수수색','규제 강화']
 
-a = ['가상화폐 규제','비트코인','가상화폐']
+database_filename = 'test.db'
+conn = lite.connect(database_filename)
+cs = conn.cursor()
 
+query = "DROP TABLE IF EXISTS t1"
+cs.execute(query)
 
-# database_filename = 'test.db'
-# conn = lite.connect(database_filename)
-# cs = conn.cursor()
+query = 'CREATE TABLE IF NOT EXISTS t1 (id INTEGER PRIMARY_KEY NOT_NULL, Google_Num INTEGER , at DATETIME)'
+cs.execute(query)
 
-# query = "DROP TABLE IF EXISTS t1"
-# cs.execute(query)
-
-# query = 'CREATE TABLE IF NOT EXISTS t1 (id INTEGER PRIMARY_KEY NOT_NULL, Num INTEGER , at DATETIME)'
-# cs.execute(query)
-
-
+b = 0
+id_num = 0
 def Google():
-    count_time = 0
-    # 5분 이내인 기사들 갯수 저장하는 데 쓰이는 거라서 Google()함수 실행할 때 처음에만 초기화
 
-    for i in range(len(a)):
+    google_count = 0
+
+    for i in range(len(keyword)):
+
         firsttime = True
-        # 항상 키워드 첫 검색할 때는 처음 페이지임
-
         url = 'https://www.google.co.kr/search?q={0}&dcr=0&tbm=nws&source=lnt&tbs=qdr:h&sa=X&ved=0ahUKEwjGzri1gJ7ZAhVCU7wKHXEVDk4QpwUIHQ&biw=645&bih=702&dpr=2'
-        # 검색어 바뀔 때마다 검색용 url 초기화
 
         while bool(url) == True:
-            # url이 존재 하면(검색할 url이 있다면)
-
             if firsttime == True:
-                # 첫번째 페이지일 때 초기화 된 url 써야됨
-                html = requests.get(url.format(a[i])).text
+                html = requests.get(url.format(keyword[i])).text
                 soup = BeautifulSoup(html, 'html.parser')
                 check = soup.select('#foot a')
                 lists = soup.select('#res')
-                # check / lists 둘 다 필요함
-                # 하나는 다음 페이지가 있는지 검사하고, 다음 페이지 검색할 때 쓰이고
-                # 다른 하나는 페이지 내에 기사 시간 가져오기 위해 필요함
                 url = ""
-                # 필요에 따라 url 지정해 주기 위해 비워줌
 
                 for title in lists:
                     x = title.select('.f')
-                    # 이 부분이 시간있는 태그 검사하는 곳
                     for time1 in x:
-                        # print(time1.text[-5:])
                         p1 = re.compile('[0-5]+분 전')
                         m1 = p1.search(time1.text[-5:])
-                        # p2 = re.compile('[0-9]{0,2}초 전')
-                        # m2 = p2.search(time1.text[-5:])
-                        # 초 단위도 할 것인가?
-
                         if (m1 != None) and (len(time1.text[-5:].strip()) == 4):
-                            count_time += 1
-                            # 구글은 마지막 자리에 시간이 들어가서
-                            # 마지막만 4~5자리만 검색해 주면 됨
-                            # 정규표현식대로 ~분 전 으로 검색해서 한자리 수라면 맨 앞자리가 null일테니 공백 지우는 strip()써주면 4자리가 될 것이기 때문에 저렇게 써줌
-                            # 그래서 해당되면 5분 내 작성된 기사 개수 +1
-
+                            google_count += 1
                 firsttime = False
-                # 첫번째 페이지 검색 끝났으니까 False로 돌려줌
-                # 밑 부분은 할 필요없음
-
 
             elif firsttime == False:
-                # 두번째 페이지부터는 맨 밑에 '다음'이라는 버튼이 있는 url로 가야됨
                 html = requests.get(url).text
                 soup = BeautifulSoup(html, 'html.parser')
                 check = soup.select('#foot a')
                 lists = soup.select('#res')
                 url = ""
 
-
                 for title in lists:
                     x = title.select('.f')
                     for time1 in x:
-                        # print(time1.text[-5:])
+
                         p1 = re.compile('[0-5]+분 전')
-                        m1 = p1.search(time1.text[-5:])
-                        # p2 = re.compile('[0-9]{0,2}초 전')
-                        # m2 = p2.search(time1.text[-5:])
-                        # 초 단위도 할 것인가?
 
                         if (m1 != None) and (len(time1.text[-5:].strip()) == 4):
-                            count_time += 1
-
-
+                            google_count += 1
 
             ctx = '다음'
             for ini in check:
                 if ctx in ini.text:
                     url = "https://www.google.co.kr" + ini.get('href')
-                    # 다음페이지 저장하기
+
+    return google_count
 
 
-        print("*********{0}**********".format(a[i]))
-        # 지금까지 어떤 키워드 검색했는지 알려주기
+while True:
+    query = "INSERT into t1 values (?, ?, DATETIME('NOW','LOCALTIME'))"
+    cs.execute(query, (b, Google()))
+    b += 1
 
+    first = cs.execute("SELECT at from t1 limit 1")
+    for row1 in first:
+        row_first = datetime.strptime(row1[0], '%Y-%m-%d %H:%M:%S')
 
+    last = cs.execute("SELECT at from t1 ORDER BY at DESC limit 1")
+    for row2 in last:
+        row_last = datetime.strptime(row2[0], '%Y-%m-%d %H:%M:%S')
 
+    diff = row_last-row_first
 
+    if diff > timedelta(minutes=2):
+        query = "DELETE from t1 where id=?"
+        cs.execute(query, (id_num,))
+        id_num += 1
 
-# ========================================== while ============================================
-    print("\nFinished")
-    print(count_time)
+    conn.commit()
+    time.sleep(5)
 
-    return count_time
-
-
-
-Google()
-
-
-# query = "INSERT into t1 values (?, ?, DATETIME('NOW','LOCALTIME'))"
-# cs.execute(query, (b, Google()))
-# conn.commit()
-
-# cs.close()
-# conn.close()
+cs.close()
+conn.close()
